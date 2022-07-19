d1383a961b7f727a906dfc7d2f502a79cf0955dc
Aqeel Ahmed <106437689+AhmecC@users.noreply.github.com>
2022-07-19 18:32:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.common import ElementClickInterceptedException, NoSuchElementException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+import random
+
+# -------------------- CONSTANTS -------------------- #
+path = r"CHROMEDRIVER LOCATION"
+LOGIN = "YOUR FB LOGIN"
+PASS = "YOUR FB PASS"
+
+# -------------------- CONNECT TO TINDER -------------------- #
+driver = webdriver.Chrome(executable_path=path)
+driver.get("https://tinder.com/")
+sleep(0.5)
+
+
+# -------------------- DIRECT TO FACEBOOK LOGIN -------------------- #
+driver.find_element(By.XPATH, '//*[@id="s2097736098"]/div/div[2]/div/div/div[1]/div[1]/button').click()
+# Accepts Tinder Cookies
+
+driver.find_element(By.XPATH, '//*[@id="s2097736098"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a').click()
+sleep(0.5)  # Finds the Login Button on Main Page
+
+driver.find_element(By.XPATH, '//*[@id="s369355022"]/div/div/div[1]/div/div/div[3]/span/div[2]/button').click()
+sleep(1.5)  # Chooses Facebook as Login Option
+
+
+# -------------------- LOGIN VIA FACEBOOK-------------------- #
+base_window, fb_window = driver.window_handles[0], driver.window_handles[1]
+driver.switch_to.window(fb_window)  # Switch Window Handle to Facebook-Popup Login
+
+driver.find_elements(By.XPATH, "//button[contains(string(), 'Allow essential and optional cookies')]")[0].click()
+sleep(1)  # Accepts Cookies on Facebook Popup
+
+driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(LOGIN)
+# Enters your facebook email/number
+
+driver.find_element(By.XPATH, '//*[@id="pass"]').send_keys(PASS)
+# Enters your facebook password
+
+driver.find_element(By.XPATH, '//*[@id="loginbutton"]').click()
+sleep(5)  # Confirms Login
+
+
+# -------------------- ACCEPT PREFERENCES TINDER -------------------- #
+driver.switch_to.window(base_window)  # Switch Window Handle to Tinder
+
+driver.find_element(By.XPATH, '//*[@id="s369355022"]/div/div/div/div/div[3]/button[1]').click()
+sleep(0.5)  # Allows Location
+
+driver.find_element(By.XPATH, '//*[@id="s369355022"]/div/div/div/div/div[3]/button[2]').click()
+sleep(7)  # Disables Notifications
+
+
+# -------------------- START MATCHING -------------------- #
+while True:
+    sleep(random.randint(1, 3))
+    try:
+        print("Swiping")
+        like = driver.find_element(By.XPATH, '//body')
+        like.send_keys(Keys.ARROW_RIGHT)
+    except ElementClickInterceptedException:
+
+        try:
+            print("Popup")
+            driver.find_element(By.CSS_SELECTOR, ".ItsAMatch a").click()
+        except NoSuchElementException:
+            print("took too long to load")
+            sleep(2)
+        except:
+            driver.find_element(By.XPATH, '//*[@id="s369355022"]/div/div/div[3]/button[2]').click()
+            print("Out of Likes")
+            break
+
+
